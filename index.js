e8287d56e8865b51f4caf49ae5d30e138aa7ab61
masterprog-cmd <xaviabadgome01@gmail.com>
2022-06-14 12:56:54 +0200

    config

--- index.js
+++ index.js
@@ -14,15 +14,13 @@ app.use(cors())
 app.use(express.json())
 app.use(logger)
 
-let notes = []
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
+    response.send(notes)
   })
 })
 
--- mongo.js
+++ mongo.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://masterprog-cmd:HkT91o52@cluster0.yoprb.mongodb.net/?retryWrites=true&w=majority';
+const connectionString = 'mongodb+srv://masterprog-cmd:HkT91o52@cluster0.yoprb.mongodb.net/masterdb?retryWrites=true&w=majority';
 
 //Conexión mongoDB
 mongoose.connect(connectionString)
     .then(() => {
         console.log('Connected to database!');
-
     })
     .catch(() => {
         console.log('Connection failed!');
